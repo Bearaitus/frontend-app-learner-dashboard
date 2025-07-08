05b57fa686c2469715f37e76c11a69884be55320
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-08 13:03:33 +0300

    Добавление размера картинке

--- src/containers/CoursesPanel/NoCoursesView/index.jsx
+++ src/containers/CoursesPanel/NoCoursesView/index.jsx
@@ -18,7 +18,7 @@ export const NoCoursesView = () => {
       id="no-courses-content-view"
       className="d-flex align-items-center justify-content-center mb-4.5"
     >
-      <Image src={emptyCourseSVG} alt={formatMessage(messages.bannerAlt)} />
+      <Image src={emptyCourseSVG} alt={formatMessage(messages.bannerAlt)} width="500px" />
       <h1>
         Ищете новый вызов?
       </h1>
